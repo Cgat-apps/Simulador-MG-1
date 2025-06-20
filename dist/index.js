de55c3c3e6dca13fede4a684bc12e94285645bbd
Segatti99 <carlos.guilherme99@hotmail.com>
2025-06-19 21:39:27 -0300

    Updated index.js

--- dist/index.js
+++ dist/index.js
@@ -324,7 +324,12 @@ var vite_config_default = defineConfig({
   root: path.resolve(import.meta.dirname, "client"),
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: void 0
+      }
+    }
   },
   server: {
     fs: {
